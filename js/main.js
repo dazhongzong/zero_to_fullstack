91bbc9c1b6cc0d9dce9eddd0ee4a30fb4b5fc628
lsz <1742428999@qq.com>
2026-09-01 14:17:33 +0800

    module

---
+++ js/main.js
@@ -0,0 +1,6 @@
+import { initNav } from "./nav.js";
+import { initCardsAnim } from "./cards.js";
+import { initScoreAnim } from "./score.js";
+initNav();
+initCardsAnim();
+initScoreAnim();
--- script.js
+++
@@ -1,5 +0,0 @@
-let count = 0;
-function changeText() {
-    count++;
-    document.getElementById('msg').textContent = `你刚刚触发了一段 TypeScript。这是第 ${count} 次点击。`;
-}
